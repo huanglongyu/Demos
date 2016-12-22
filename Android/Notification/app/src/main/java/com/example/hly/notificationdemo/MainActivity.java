11c130adb75e9dd22fb0b3c44b6faa4d4cd81ced
longyu.huang <longyu.huang2014@gmail.com>
2016-12-22 04:12:59 -0800

    [Notification]add big view test[2/2]

--- Android/Notification/app/src/main/java/com/example/hly/notificationdemo/MainActivity.java
+++ Android/Notification/app/src/main/java/com/example/hly/notificationdemo/MainActivity.java
@@ -3,10 +3,8 @@ package com.example.hly.notificationdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.RemoteViews;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
