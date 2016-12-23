684d045d3b52bf755b68277dd7b3aa0da36296e6
longyu.huang <longyu.huang2014@gmail.com>
2016-12-23 02:34:34 -0800

    [Notification]change ActivityA behavior to adapter LuckyMoney demo

--- Android/Notification/app/src/main/java/com/example/hly/notificationdemo/ActivityA.java
+++ Android/Notification/app/src/main/java/com/example/hly/notificationdemo/ActivityA.java
@@ -2,18 +2,27 @@ package com.example.hly.notificationdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
  * Created by hly on 11/27/16.
  */
 
-public class ActivityA extends Activity {
+public class ActivityA extends Activity implements View.OnClickListener{
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView view = new TextView(this);
-        view.setText("ActivityA");
-        setContentView(view);
+        button = new Button(this);
+        button.setText("ActivityA");
+        button.setOnClickListener(this);
+        setContentView(button);
+    }
+
+    @Override
+    public void onClick(View view) {
+        button.setText("Onclicked");
     }
 }
